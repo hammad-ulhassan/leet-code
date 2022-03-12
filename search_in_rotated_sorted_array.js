9bdae7ac0ba1c5f6b471c95f13b0f412f5a2b660
Hammad Hassan <i121976@nu.edu.pk>
2022-03-12 17:00:35 +0500

    search in rotated sorted array

---
+++ search_in_rotated_sorted_array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  var i = -1;
+  var t = -1;
+  var find = function (arr, n) {
+    if (arr.length == 1) {
+      i += 1;
+      if (arr[0] == n) {
+        t = i;
+        return;
+      }
+      return;
+    }
+    find(arr.slice(0, arr.length / 2), n);
+    find(arr.slice(arr.length / 2), n);
+  };
+  find(nums, target);
+  return t;
+};
